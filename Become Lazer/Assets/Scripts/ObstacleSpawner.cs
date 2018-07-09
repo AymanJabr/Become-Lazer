45d3bb79a3d91990e025b6953d32aa349fb0d7a0
AymanJabr <40807842+AymanJabr@users.noreply.github.com>
2018-07-09 16:26:59 +0300

    Working Test Game 01

---
+++ Become Lazer/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+
+	public GameObject Obstacle;
+
+	public float TimeUntilNextObstacle;
+
+	private float XPosSpawnPoint;
+	private Transform PositionOfSpawn;
+
+	// Use this for initialization
+	void Start () {
+		PositionOfSpawn = gameObject.transform;
+		InvokeRepeating ("SpawnObstacle", 1f, TimeUntilNextObstacle);
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		//StartCoroutine (SpawnObstacle ());
+	}
+
+
+	void SpawnObstacle(){
+		Vector3 Pos;
+
+		Pos.x = Random.Range (-2, 2);
+		Pos.y = PositionOfSpawn.position.y;
+		Pos.z = PositionOfSpawn.position.z;
+
+
+		Instantiate (Obstacle, Pos, Quaternion.identity);
+	}
+
+	/*IEnumerator SpawnObstacle(){
+		while (true) {
+			Vector3 Pos;
+
+			Pos.x = Random.Range (-2, 2);
+			Pos.y = PositionOfSpawn.position.y;
+			Pos.z = PositionOfSpawn.position.z;
+
+
+			Instantiate (Obstacle, Pos, Quaternion.identity);
+
+
+			yield return new WaitForSeconds (TimeUntilNextObstacle);
+		}
+	}*/
+}
---
+++ Become Lazer/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("Collided with particles");
+
+		Player.GetComponent<TrailRendererTest> ().lifeTime = 0f;
+		StartCoroutine (DestroyPlayer ());
+	}
+
+	IEnumerator DestroyPlayer(){
+
+		yield return new WaitForSeconds (0.08f);
+		Destroy(Player);
+	}
+}
