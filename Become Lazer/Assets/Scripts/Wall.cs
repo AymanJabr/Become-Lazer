f73224ce3e4f9b8fedb65351d40ad699ebb1e4bb
AymanJabr <40807842+AymanJabr@users.noreply.github.com>
2018-07-10 17:39:10 +0300

    decorative changes

---
+++ Become Lazer/Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	public GameObject Player;
+	public Rigidbody2D PlayerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		PlayerRigidBody = Player.GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/*void OnTriggerEnter2D(){
+		PlayerRigidBody.velocity = Vector2.zero;
+	}*/
+}
