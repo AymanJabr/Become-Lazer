170c0106b635cd6f0f91d6d07190f1aa8429013a
ahm0162017 <ahm0162017@users.noreply.github.com>
2018-07-16 17:57:03 +0300

    change the pointer

---
+++ Become Lazer/Assets/Scripts/Point.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+    public GameObject Pointer;
+    float distance;
+    public float PointerLength,PointerSpeed;
+    float alpha = 0.5f; 
+    int OneTime = 1;
+    public MeshRenderer color;
+	// Use this for initialization
+	void Start () {
+        Pointer = GameObject.Find("animation");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(PointerSpeed * Time.deltaTime, 0, 0);
+        distance = Vector2.Distance(transform.position, Pointer.transform.position);
+
+        if ((distance > PointerLength) && (OneTime == 1))
+        {
+            var Pointer1 = Pointer.GetComponent("PointerAnimation") as PointerAnimation;
+            Pointer1.ins = true;
+            OneTime = 0;
+        }
+        alpha -= 0.01f;
+          GetComponent <SpriteRenderer>().color = new Color(1,1,1, alpha);
+        if (alpha < 0) gameObject.SetActive(false);
+    }
+}
---
+++ Become Lazer/Assets/Scripts/PointerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerAnimation : MonoBehaviour {
+    public GameObject Lpoint,Rpoint;
+    public bool ins = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ins == true)
+        {
+            Instantiate(Lpoint,transform.position,transform.rotation);
+            Instantiate(Rpoint, transform.position, transform.rotation);
+
+            ins = false;
+        }
+
+
+    }
+}
