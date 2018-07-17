6f77aa248fa4c13076b05f6943d8cf2fb4dce49f
ahm0162017 <40832430+ahm0162017@users.noreply.github.com>
2018-07-17 10:26:36 +0300

    add some comments

--- Become Lazer/Assets/Scripts/Point.cs
+++ Become Lazer/Assets/Scripts/Point.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class Point : MonoBehaviour {
     public GameObject Pointer;
-    float distance;
+	
+    float distance,alpha;
+	
     public float PointerLength,PointerSpeed;
-    float alpha = 0.5f; 
+	
     int OneTime = 1;
-    public MeshRenderer color;
-	// Use this for initialization
+	
 	void Start () {
         Pointer = GameObject.Find("animation");
+   	alpha = 0.5f ;
 	}
 
-    // Update is called once per frame
     void Update() {
-        transform.Translate(PointerSpeed * Time.deltaTime, 0, 0);
-        distance = Vector2.Distance(transform.position, Pointer.transform.position);
+	    
+        transform.Translate(PointerSpeed * Time.deltaTime, 0, 0);//move the point
+        distance = Vector2.Distance(transform.position, Pointer.transform.position); 
 
         if ((distance > PointerLength) && (OneTime == 1))
         {
-            var Pointer1 = Pointer.GetComponent("PointerAnimation") as PointerAnimation;
-            Pointer1.ins = true;
+            var Pointer1 = Pointer.GetComponent("PointerAnimation") as PointerAnimation; 
+            Pointer1.ins = true; //change the boolean 'ins' in PointerAnimation script 
             OneTime = 0;
         }
+	    
         alpha -= 0.01f;
-          GetComponent <SpriteRenderer>().color = new Color(1,1,1, alpha);
+          GetComponent <SpriteRenderer>().color = new Color(1,1,1, alpha); // CHANGE ALPHA 
         if (alpha < 0) gameObject.SetActive(false);
     }
 }
