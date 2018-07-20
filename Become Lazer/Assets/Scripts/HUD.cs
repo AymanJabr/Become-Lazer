d2076cdbb085b224bb511be8bfdc521cdc05528a
ahm0162017 <40832430+ahm0162017@users.noreply.github.com>
2018-07-20 15:27:21 +0300

    Score #1

---
+++ Become Lazer/Assets/Scripts/HUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HUD : MonoBehaviour {
+	public GameObject cam;
+	public int PlayerScore ;
+	public Text ScoreText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Score ();
+	}
+
+	void Score () {
+		PlayerScore = Mathf.RoundToInt(cam.transform.position.y/2f);
+		ScoreText.text = "Score :" + PlayerScore.ToString ();
+
+		//PlayerScore = cam.transform.position.y;
+	}
+}
