71959a1af778604c1e14bbf9c4c84aecef309704
ahm0162017 <40832430+ahm0162017@users.noreply.github.com>
2018-07-20 12:00:15 +0300

    finishing the pointer

--- Become Lazer/Assets/Scripts/PointerAnimation.cs
+++ Become Lazer/Assets/Scripts/PointerAnimation.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PointerAnimation : MonoBehaviour {
     public GameObject Lpoint,Rpoint;
     public bool ins = true;
-	// Use this for initialization
+	public bool ShowPointer = false ;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (ins == true)
+		
+
+		if (ins == true && ShowPointer == true )
         {
             Instantiate(Lpoint,transform.position,transform.rotation);
             Instantiate(Rpoint, transform.position, transform.rotation);
@@ -20,6 +23,10 @@ public class PointerAnimation : MonoBehaviour {
             ins = false;
         }
 
-
     }
+
+	public void Show(){
+		ShowPointer = !ShowPointer;
+
+	}
 }
