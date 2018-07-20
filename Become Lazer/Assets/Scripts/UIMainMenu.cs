4fb3f37febdc165648d6e1ff5acbe015f88c97b5
AymanJabr <40807842+AymanJabr@users.noreply.github.com>
2018-07-20 13:27:57 +0300

    added volume slider

---
+++ Become Lazer/Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour {
+
+	private GameObject OptionsMenu;
+
+	// Use this for initialization
+	void Start () {
+		OptionsMenu = GameObject.Find ("OptionsMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ExitGame(){
+
+		Application.Quit ();
+	}
+
+	public void PlayGame(){
+		// add script to go to the main game. SceneManager.loadScene();
+	}
+
+	public void Upgrades(){
+		//add upgrades code here.
+	}
+		
+}
+
